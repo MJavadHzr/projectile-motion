257af225b6f372a890c2d669bfd124707a843303
MJavad Hezareh <55049080+MJavadHzr@users.noreply.github.com>
2020-10-20 20:46:15 +0330

    drag-coefficient service

---
+++ drag-coefficient.py
@@ -0,0 +1,24 @@
+from reader import Reader
+
+
+class DragCoefficientService:
+    def __init__(self):
+        self.air_velocity = 330
+        self.reader = Reader()
+        self.x_data = []
+        self.y_data = []
+
+    def set_graph_data(self):
+        self.reader.read_graph()
+        self.x_data = self.reader.get_x_data()
+        self.y_data = self.reader.get_y_data()
+
+    def get_coefficient(self, velocity):
+        mach_number = velocity / self.air_velocity
+        index = self.get_index(mach_number)
+        return self.y_data[index]
+
+    def get_index(self, value):
+        for i in range(len(self.x_data) - 1):
+            if (self.x_data[i] < value) & (value < self.x_data[i + 1]):
+                return i
