19e432d4c8cdcd50d0c809b66c5da4e4f5154f28
MJavad Hezareh <55049080+MJavadHzr@users.noreply.github.com>
2020-10-20 20:30:18 +0330

    graph reader

---
+++ reader.py
@@ -0,0 +1,17 @@
+class Reader:
+    def __init__(self):
+        self.graph_addr = "drag-coefficient.txt"
+        self.x_axis = []
+        self.y_axis = []
+
+    def read_graph(self):
+        reader = open(self.graph_addr)
+        for line in reader.readlines():
+            self.x_axis.append(line.split(':+++:')[0])
+            self.y_axis.append(line.split(':+++:')[1])
+
+    def get_x_data(self):
+        return self.x_axis
+
+    def get_y_data(self):
+        return self.y_axis
