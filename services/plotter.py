6a781d1115bc67ec3d6a23208217d86fee90de30
MJavad Hezareh <55049080+MJavadHzr@users.noreply.github.com>
2020-10-21 11:33:25 +0330

    yet refactor 🙄

--- services/plotter.py
+++ services/plotter.py
@@ -1,12 +1,20 @@
 import matplotlib.pyplot as plt
+import os
 
 
 class Plotter:
+    output_addr = "output"
 
-    @staticmethod
-    def plot(y_axis, x_axis, y_label, x_label, name):
+    @classmethod
+    def plot(cls, y_axis, x_axis, y_label, x_label, name):
         plt.plot(x_axis, y_axis)
         plt.xlabel(x_label)
         plt.ylabel(y_label)
-        plt.savefig("output/" + name)
+        cls.__check_dir()
+        plt.savefig(cls.output_addr + "/" + name)
         plt.show()
+
+    @classmethod
+    def __check_dir(cls):
+        if not os.path.exists(cls.output_addr):
+            os.mkdir(cls.output_addr)
--- services/reader.py
+++ services/reader.py
@@ -1,11 +1,12 @@
 class Reader:
+    graph_data_addr = "resources/drag-coefficient.txt"
+
     def __init__(self):
-        self.graph_addr = "resources/drag-coefficient.txt"  # bad coding :\
         self.x_axis = []
         self.y_axis = []
 
     def read_graph(self):
-        reader = open(self.graph_addr)
+        reader = open(self.graph_data_addr)
         for line in reader.readlines():
             self.x_axis.append(float(line.split(':+++:')[0]))
             self.y_axis.append(float(line.split(':+++:')[1]))
